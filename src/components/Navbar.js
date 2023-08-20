d9eea34396a93e88a8058103ca901c9f13bf24d0
OwenAnderson <owen_anderson@brown.edu>
2023-08-20 18:27:29 -0400

    adding to git

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { Button } from "./Button";
+
+function Navbar() {
+  const [button, setButton] = useState(true);
+  const handleClick = () => {
+    console.log("clicked");
+  };
+
+  // note: don't have any of the rescaling stuff for the button
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            Hoppi
+          </Link>
+          {button && <Button buttonStyle="btn--outline">JOIN</Button>}
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
