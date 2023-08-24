33cada334626f8ee1342bb5b987fd18a47541101
OwenAnderson <owen_anderson@brown.edu>
2023-08-24 12:15:01 -0400

    formetted one app panel

--- src/components/Appdescription.js
+++ src/components/Appdescription.js
@@ -4,19 +4,25 @@ import "./Appdescription.css";
 function Appdescription() {
   return (
     <div className="description-container">
-      <div class="image-container">
-        <img class="image-mocks" src="images/fayble-map-mockup.png" alt="phone" />
-      </div>
-      <div className="text-container">
-        <h1 class="headers">Always</h1>
-        <h2 class="headers">Know</h2>
-        <h3 class="headers">What's</h3>
-        <h4 class="headers">Going</h4>
-        <h5 class="headers">On</h5>
-        <p>
-          Plan, atttend, explore, and share your favrotie events with your
-          entire community (or just your friends)!
-        </p>
+      <div class="map-logic-container">
+        <div class="image-container">
+          <img
+            class="image-mocks"
+            src="images/fayble-map-mockup.png"
+            alt="phone"
+          />
+        </div>
+        <div className="text-container">
+          <h1 class="headers">Always</h1>
+          <h2 class="headers">Know</h2>
+          <h3 class="headers">What's</h3>
+          <h4 class="headers">Going</h4>
+          <h5 class="headers">On</h5>
+          <p>
+            Plan, atttend, explore, and share your favrotie events with your
+            entire community (or just your friends)!
+          </p>
+        </div>
       </div>
       {/* <div class="image-container">
         <img class="image-mocks" src="images/fayble-map-mockup.png" alt="phone" />
